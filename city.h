6c8e546a8346fcd40726f4872f8d63618b60c532
MaJessica9 <jamma@ucdavis.edu>
2015-04-02 15:39:03 -0700

    Create city.h

---
+++ city.h
@@ -0,0 +1,15 @@
+// city.h - 
+#ifndef
+
+struct City
+{
+  double longitude; // float?
+  double latitude; // float?
+  char name[]; // HOW TO DO???
+  char state[]; // HOW TO DO???
+  char airport_abb[3];
+  double population; // int?
+};
+
+
+#endif
