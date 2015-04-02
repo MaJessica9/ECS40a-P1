6dcacfa580f8160e5bca49b1b1f506275486966a
MaJessica9 <jamma@ucdavis.edu>
2015-04-02 15:54:32 -0700

    Create vector.h

---
+++ vector.h
@@ -0,0 +1,11 @@
+#ifndef
+
+struct Vector
+{
+  City * cityArray; //Don't need cityArray[] because you will 
+                    // determine the size of the array later?
+  int current_size; 
+  int current_count;
+};
+
+#endif
